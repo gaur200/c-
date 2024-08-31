e6ef5cf4d2732350148980f617b2bdd8dce16e1f
gaur200 <gaurravi291@gmail.com>
2024-08-31 03:40:30 -0700

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+//     int n;
+//     cout << "enter n :";
+//     cin >> n;
+
+//     if (n % 2 == 0)
+//        {
+//          cout << "even number"<<endl;
+//        }
+
+//    else
+//         {
+//             cout<<"odd number"<<endl;
+//             cout<<"wow";
+//         }
+// }  
+         
+
+         //take integer input and print the absolute number
+
+//            int n;
+//     cout << "enter n :";
+//      cin >> n;
+
+//      if (n >=0){
+//         cout<<n;
+
+//      }
+//      else{
+//         cout<<-n;
+//         //if you want to change into abslute value
+//         if(n<0) n=-n;
+//         cout<<n;
+//      }
+//  }
+
+
+//if cost price and seeling price of an item is input the keyboard , write a program to determine whether seller has made profit or incurred loss or no profit no loss.Also determine how much profit he made or loss he incurred.
+
+//            int cp;
+//     cout << "enter cost price:";
+//       cin >> cp;
+// int sp;
+// cout<<"enter selling price ";
+// cin>>sp;
+//       if (sp>cp){
+//          cout<<"profit is "<<sp-cp;
+
+
+//       }
+//      else if
+//         (sp>cp){
+//             cout<<"loss is"<<cp-sp;
+//         }
+//       else{
+//         cout<<"no profit and no loss";
+//       }
+        
+//       }
+  
+ 
+
+ //take positive integer input and tell if it is a three digit number or not
+//logical operator
+//  int n;
+//  cout<<"enter n :";
+//  cin>>n;
+//  if(n>99 && n<1000){
+//     cout<<"3 digit number";
+
+//  }
+//  else{
+//     cout <<"not 3 digit number";
+//  }
+//  }
+
+// question :take positive integer input and tell if it is divisible by 5 and 3.
+
+// int n;
+// cout<<"enter n :";
+// cin>>n;
+// if(n%5==0 && n%3==0){
+//     cout<<"divisible by 5 and 3";
+// }
+// else{
+//     cout<<"not divisible by 5 and 3";
+// }
+//  }
+
+
+//  question :take positive integer input and tell if it is divisible by 5 or 3. logical
+//  int n;
+//  cout<<"enter n :";
+//  cin>>n;
+//  if(n%5==0 || n%3==0){
+//      cout<<"divisible by 5 and 3";
+//  }
+//  else{
+//      cout<<"not divisible by 5 and 3";
+//  }
+//   }
+
+// 3. take 3 numbers input and tell if they can be the sides of a triangle.
+
+// int a ,b,c;
+// cout<<"enter first side ";
+// cin>>a;
+// cout<<"enter second side ";
+// cin>>b;
+// cout<<"enter thide side ";
+// cin>>c;
+// if(a+b>c and b+c>a and c+a>b){
+//     cout<<"Valid Triangle ";
+// }else{
+//     cout<<"invalid triangle ";
+// }
+
+//  }
+
+//4. take 3 positive itegers input and print the greatest of them.
+int a ,b,c;
+ cout<<"enter first side ";
+ cin>>a;
+ cout<<"enter second side ";
+ cin>>b;
+ cout<<"enter thide side ";
+ cin>>c;
+ if(a>b and a>c){
+    cout<<a<<"is greatest";
+ }
+ else if
+    (b>a and b>c){
+cout<<b<<"is greatest";
+
+
+    }
+    else{
+        
+    cout<<c<<"is greatest";
+
+    }
+ }
